51241bc61730a4ba78b81c117dcc451ebb2afd2e
Emanhusseinn <eman.hussein.jo@gmail.com>
2021-01-02 11:20:06 +0200

    ADDING

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan')
 var cors = require('cors')
 var app = express();
-// const path = require('path');
+const path = require('path');
 
 //the Routes
 const authRoutes = require('./routes/auth');
@@ -28,13 +28,7 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 });
-//
-// if (process.env.NODE_ENV === 'production') {           
-//   app.use(express.static('client/build'));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
-// }
+
 
 
 // MIDDILWARES
@@ -44,16 +38,16 @@ app.use('/user',userRoute);
 app.use('/teacher',teacherRoute);
 app.use('/materials', materialsRouter);
 
-// // serve static assets if were in production 
-// if(process.env.NOD_ENV === 'production'){
-//   // set static folder
-//   app.use(express.static('frontend/build'));
+// serve static assets if were in production 
+if(process.env.NOD_ENV === 'production'){
+  // set static folder
+  app.use(express.static('frontend/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 
-//   });
-// }
+  });
+}
 
 
 //port with whatever the port will be given by heruko
